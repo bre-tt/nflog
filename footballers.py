05ed47be951c631c08a0f699ed903649045426bc
brett mcphail <brettbrysonmcphail@gmail.com>
2024-09-28 20:10:06 -0300

    footballers.py: added incompletions
    Signed-off-by: brett mcphail <brettbrysonmcphail@gmail.com>

--- footballers.py
+++ footballers.py
@@ -15,9 +15,29 @@ def get_completion_count(team_id):
     #print(f"The number of times IsPass is 1 when OffenseTeam is KC is: {count}")
     #print(f"The number of completed passes for Kansas City on offense is {kc_completion_count}")
 
+def get_incompletion_count(team_id):
+
+    # Filter the DataFrame to include only rows where OffenseTeam is 'KC'
+    team_df = df[df['OffenseTeam'] == team_id]
+    incompletion_count = df['IsPass'].sum() + df['IsIncomplete'].sum()
+    team_incompletion_count = team_df['IsPass'].sum() + team_df['IsIncomplete'].sum()
+    return team_incompletion_count
+    # Count the number of times IsPass is 1 in the filtered DataFrame
+    #count = team_df['IsPass'].sum()
+    #print(f"The number of times IsPass is 1 when OffenseTeam is KC is: {count}")
+    #print(f"The number of completed passes for Kansas City on offense is {kc_completion_count}")
+
 if __name__ == "__main__": 
 
     parser = argparse.ArgumentParser(prog="footballers")
     parser.add_argument("team_id")
     args = parser.parse_args()
     print(f"{args.team_id} has {get_completion_count(args.team_id)} completions")
+
+if __name__ == "__main__": 
+
+    parser = argparse.ArgumentParser(prog="footballers")
+    parser.add_argument("team_id")
+    args = parser.parse_args()
+    print(f"{args.team_id} has {get_incompletion_count(args.team_id)} incompletions")
+
