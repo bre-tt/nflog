2f74ceb7d648db0363ee19ce27071a3777c0ed43
brett mcphail <brettbrysonmcphail@gmail.com>
2024-09-27 22:08:39 -0300

    footballers.py added parsers args. i'm an argsparsonaut
    Signed-off-by: brett mcphail <brettbrysonmcphail@gmail.com>

--- footballers.py
+++ footballers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 
 df = pd.read_csv('/Users/wolastoqeducation/Downloads/pbp-2024.csv')
 
@@ -6,16 +7,17 @@ def get_completion_count(team_id):
 
     # Filter the DataFrame to include only rows where OffenseTeam is 'KC'
     team_df = df[df['OffenseTeam'] == team_id]
-    print(team_df)
     completion_count = df['IsPass'].sum() - df['IsIncomplete'].sum()
-    print(completion_count)
     team_completion_count = team_df['IsPass'].sum() - team_df['IsIncomplete'].sum()
-    print(team_completion_count)
+    return team_completion_count
     # Count the number of times IsPass is 1 in the filtered DataFrame
     #count = team_df['IsPass'].sum()
     #print(f"The number of times IsPass is 1 when OffenseTeam is KC is: {count}")
     #print(f"The number of completed passes for Kansas City on offense is {kc_completion_count}")
 
-get_completion_count("KC")
-get_completion_count("PIT")
-get_completion_count("BAL")
+if __name__ == "__main__": 
+
+    parser = argparse.ArgumentParser(prog="footballers")
+    parser.add_argument("team_id")
+    args = parser.parse_args()
+    print(get_completion_count(args.team_id))
