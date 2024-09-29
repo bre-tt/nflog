c33068da966d689b4ef09a5f174d4181696e6742
brett mcphail <brettbrysonmcphail@gmail.com>
2024-09-28 21:13:18 -0300

    footballers.py: fixed offensive formation for shotguns
    Signed-off-by: brett mcphail <brettbrysonmcphail@gmail.com>

--- footballers.py
+++ footballers.py
@@ -10,10 +10,6 @@ def get_completion_count(team_id):
     completion_count = df['IsPass'].sum() - df['IsIncomplete'].sum()
     team_completion_count = team_df['IsPass'].sum() - team_df['IsIncomplete'].sum()
     return team_completion_count
-    # Count the number of times IsPass is 1 in the filtered DataFrame
-    #count = team_df['IsPass'].sum()
-    #print(f"The number of times IsPass is 1 when OffenseTeam is KC is: {count}")
-    #print(f"The number of completed passes for Kansas City on offense is {kc_completion_count}")
 
 def get_incompletion_count(team_id):
 
@@ -23,16 +19,16 @@ def get_incompletion_count(team_id):
     team_incompletion_count = team_df['IsPass'].sum() + team_df['IsIncomplete'].sum()
     return team_incompletion_count
     # Count the number of times IsPass is 1 in the filtered DataFrame
-    #count = team_df['IsPass'].sum()
     #print(f"The number of times IsPass is 1 when OffenseTeam is KC is: {count}")
     #print(f"The number of completed passes for Kansas City on offense is {kc_completion_count}")
 
 def get_oformation_count(team_id):
     
     team_df = df[df['OffenseTeam'] == team_id]
-    shotgun_count = df(['Formation'] == "SHOTGUN")
-    team_shotgun_count = team_df(['Formation'].value_counts()['SHOTGUN'])
+    #shotgun_count = df(['Formation'] == "SHOTGUN")
+    team_shotgun_count = team_df['Formation'].apply(lambda x: str(x).upper().split().count('SHOTGUN') if isinstance(x, str) else 0).sum()
     return team_shotgun_count
+
 
 if __name__ == "__main__": 
 
@@ -40,4 +36,6 @@ if __name__ == "__main__":
     parser.add_argument("team_id")
     args = parser.parse_args()
     print(f"{args.team_id} has {get_completion_count(args.team_id)} completions")
-    print(f"{args.team_id} has {get_oformation_count(args.team_id)} SHOTGUNS")
+    print(f"{args.team_id} has {get_incompletion_count(args.team_id)} incompletions")
+    print(f"{args.team_id} has {get_oformation_count(args.team_id)} SHOTGUN formations")
+
